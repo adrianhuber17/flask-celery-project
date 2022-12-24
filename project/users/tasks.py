59ae28645831a0578c7e2a55bd5c6a5c269f2e20
adrianhuber17 <adrianhuber17@gmail.com>
2022-12-24 13:45:00 -0800

    updated project/users, __init__.py to import events of socketio, added events.py to initiate instance of socketio websocket connection with a /task_status namespace and room, and updated task (celery) to include a celery signal handler to be called after celery task is executed and socketio can be called

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from celery.signals import task_postrun
 
 
 @shared_task
@@ -6,3 +7,8 @@ def divide(x, y):
     import time
     time.sleep(5)
     return x / y
+
+@task_postrun.connect
+def task_postrun_handler(task_id, **kwargs):
+    from project.users.events import update_celery_task_status
+    update_celery_task_status(task_id)
