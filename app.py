d3a80e00fc722addb4b6b2c0bc4d5e2b25d76a04
adrianhuber17 <adrianhuber17@gmail.com>
2022-12-23 16:59:10 -0800

    added a cli command in app.py to reload the celery worker when a change is made to a celery file to reload the workers using the watchgod library

--- app.py
+++ app.py
@@ -8,3 +8,16 @@ celery = ext_celery.celery
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+@app.cli.command("celery_worker")
+def celery_worker():
+    """reload workers for celery"""
+    from watchgod import run_process
+    import subprocess
+
+    def run_worker():
+        subprocess.call(
+            ["celery", "-A", "app.celery", "worker", "--loglevel=info"]
+        )
+
+    run_process("./project", run_worker)
