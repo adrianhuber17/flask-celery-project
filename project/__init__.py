d3a3d3c1efb6fb7778d2c0881e6ffccd9e2286f3
adrianhuber17 <adrianhuber17@gmail.com>
2022-12-24 13:41:55 -0800

    added flask socket.io to the requirements file, and added instance of flask socket.io to app factory on __init__ of app

--- project/__init__.py
+++ project/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_celeryext import FlaskCeleryExt 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO 
 
 from project.celery_utils import make_celery
 from project.config import config
@@ -13,6 +14,7 @@ from project.config import config
 db = SQLAlchemy()
 migrate = Migrate()
 ext_celery = FlaskCeleryExt(create_celery_app=make_celery)
+socketio = SocketIO()
 
 def create_app(config_name=None):
 
@@ -29,6 +31,7 @@ def create_app(config_name=None):
     db.init_app(app)
     migrate.init_app(app, db)
     ext_celery.init_app(app)
+    socketio.init_app(app, message_queue=app.config['SOCKETIO_MESSAGE_QUEUE'])
 
     #register blueprints
     from project.users import users_blueprint
