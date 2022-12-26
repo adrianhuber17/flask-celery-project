b1b34efe7f9ba49e42daa313bdb8fe07b9ee434b
adrianhuber17 <adrianhuber17@gmail.com>
2022-12-26 11:24:36 -0800

    added celery tasks to make api calls and handle error to retry api call

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -1,5 +1,12 @@
 from celery import shared_task
-from celery.signals import task_postrun
+import random
+
+import requests
+from celery import shared_task
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+# from celery.signals import task_postrun
 
 
 @shared_task
@@ -14,7 +21,20 @@ def sample_task(email):
 
     api_call(email)
 
-@task_postrun.connect
-def task_postrun_handler(task_id, **kwargs):
-    from project.users.events import update_celery_task_status
-    update_celery_task_status(task_id)
+@shared_task(bind=True)
+def task_process_notification(self):
+    try:
+        if not random.choice([0, 1]):
+            # mimic random error
+            raise Exception()
+
+        # this would block the I/O
+        requests.post('https://httpbin.org/delay/5')
+    except Exception as e:
+        logger.error('exception raised, it would be retry after 5 seconds')
+        raise self.retry(exc=e, countdown=5)
+
+# @task_postrun.connect
+# def task_postrun_handler(task_id, **kwargs):
+#     from project.users.events import update_celery_task_status
+#     update_celery_task_status(task_id)
