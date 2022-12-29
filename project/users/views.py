41d89a8cdf372239ced06d272b881e57a1d44830
adrianhuber17 <adrianhuber17@gmail.com>
2022-12-29 11:49:56 -0800

    worked on database transactions and celery. Updated views.py to add a route to create a user in the database and send a welcome email with a task. updated tasks.py to create a task to send user an email only after they have been commited successfully to the database. updated celery_utils.py to have a flask app context class to work with celery app

--- project/users/views.py
+++ project/users/views.py
@@ -1,14 +1,16 @@
 import random
 import logging
+from string import ascii_lowercase
 
 import requests
 from celery.result import AsyncResult
 from flask import Blueprint, render_template, flash, abort, request, Response, jsonify, current_app
 
 from . import users_blueprint
-from project import csrf
+from project import csrf,db
 from project.users.forms import YourForm
-from project.users.tasks import sample_task, task_process_notification
+from project.users.models import User
+from project.users.tasks import sample_task, task_process_notification, task_send_welcome_email
 
 
 def api_call(email):
@@ -76,3 +78,25 @@ def subscribe_ws():
             'task_id': task.task_id,
         })
     return render_template('form_ws.html', form=form)
+
+def random_username():
+    username = ''.join([random.choice(ascii_lowercase) for i in range(5)])
+    return username
+
+@users_blueprint.route('/transaction_celery/', methods=('GET', 'POST'))
+def transaction_celery():
+    try:
+        username = random_username()
+        user = User(
+            username=f'{username}',
+            email=f'{username}@test.com',
+        )
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        raise
+
+    current_app.logger.info(f'user {user.id} {user.username} is persistent now')
+    task_send_welcome_email.delay(user.id)
+    return 'done'
